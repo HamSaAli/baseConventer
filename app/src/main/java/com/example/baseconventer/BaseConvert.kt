315fd67471db1111d0032673e18abfe1ce73823b
hamsa <hamsaalsaadie@gmail.com>
2023-03-12 09:28:59 +0300

    fix bugs

--- app/src/main/java/com/example/baseconventer/BaseConvert.kt
+++
@@ -1,8 +0,0 @@
-package com.example.baseconventer
-
-enum class BaseConvert {
-    BINARY,
-    DECIMAL,
-    HEXADECIMAL,
-    OCTAL
-}
--- app/src/main/java/com/example/baseconventer/MainActivity.kt
+++ app/src/main/java/com/example/baseconventer/MainActivity.kt
@@ -5,94 +5,59 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val binaryOutput by lazy { findViewById<TextView>(R.id.binaryOutput) }
-        val octalOutput by lazy { findViewById<TextView>(R.id.octalOutput) }
-        val decimalOutput by lazy { findViewById<TextView>(R.id.decimalOutput) }
-        val hexadecimalOutput by lazy { findViewById<TextView>(R.id.hexadecimalOutput) }
-        val inputField by lazy { findViewById<EditText>(R.id.inputField) }
+        val inputField =  findViewById<EditText>(R.id.inputField)
+        val baseRadioGroup =  findViewById<RadioGroup>(R.id.baseRadioGroup)
 
 
-        class BaseTextWatcher : TextWatcher {
-
-            private var prevInputStr = ""
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val inputStr = s.toString().lowercase()
-                if (inputStr != prevInputStr) {
-                    val filteredStr = inputStr.filter {
-                        it !in setOf(
-                            'g',
-                            'h',
-                            'i',
-                            'j',
-                            'k',
-                            'l',
-                            'm',
-                            'n',
-                            'o',
-                            'p',
-                            'q',
-                            'r',
-                            's',
-                            't',
-                            'u',
-                            'v',
-                            'w',
-                            'x',
-                            'y',
-                            'z'
-                        )
-                    }
-                    prevInputStr = filteredStr
-                    (s as Editable).replace(0, s.length, filteredStr)
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                val inputStr = s.toString()
-
-                if (inputStr.isNotEmpty()) {
-                    var inputInt = 0
-
-                    for (c in inputStr) {
-                        if (c.isDigit()) {
-                            inputInt = inputInt * 10 + c.toString().toInt()
-                        } else {
-                            val charToIntMap = mapOf(
-                                'a' to 10, 'b' to 11, 'c' to 12,
-                                'd' to 13, 'e' to 14, 'f' to 15
-                            )
-
-                            val charValue = charToIntMap[c.lowercaseChar()]
-                            if (charValue != null) {
-                                inputInt = inputInt * 16 + charValue
-                            }
-                        }
-                    }
-
-                    binaryOutput.text = inputInt.toString(2)
-                    octalOutput.text = inputInt.toString(8)
-                    decimalOutput.text = inputInt.toString(10)
-                    hexadecimalOutput.text = prevInputStr
-
-                } else {
-                    binaryOutput.text = ""
-                    octalOutput.text = ""
-                    decimalOutput.text = ""
-                    hexadecimalOutput.text = ""
-                }
-            }
+        baseRadioGroup.setOnCheckedChangeListener { _, _ ->
+            updateOutputs(inputField.text.toString())
         }
-        inputField.addTextChangedListener(BaseTextWatcher())
+
+
+        inputField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                updateOutputs(s.toString())
+            }
+        })
     }
 
+    private fun updateOutputs(inputStr: String) {
+        val baseRadioGroup = findViewById<RadioGroup>(R.id.baseRadioGroup)
+        val binaryOutput = findViewById<TextView>(R.id.binaryOutput)
+        val octalOutput = findViewById<TextView>(R.id.octalOutput)
+        val decimalOutput = findViewById<TextView>(R.id.decimalOutput)
+        val hexadecimalOutput = findViewById<TextView>(R.id.hexadecimalOutput)
+
+
+        val selectedBase = when (baseRadioGroup.checkedRadioButtonId) {
+            R.id.binaryRadio -> 2
+            R.id.octalRadio -> 8
+            R.id.decimalRadio -> 10
+            R.id.hexadecimalRadio -> 16
+            else -> 10
+        }
+
+
+        val inputInt = try {
+            inputStr.toLong(selectedBase).toInt()
+        } catch (e: NumberFormatException) {
+            0
+        }
+
+        binaryOutput.text = inputInt.toString(2)
+        octalOutput.text = inputInt.toString(8)
+        decimalOutput.text = inputInt.toString(10)
+        hexadecimalOutput.text = inputInt.toString(16)
+    }
 }
